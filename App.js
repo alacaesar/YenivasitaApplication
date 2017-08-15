d9c26db1e263b4290f43e4414ce4c0664cc6bf8a
Alaa Muhammed <alacaesar@gmail.com>
2017-08-15 03:14:07 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import {
+  StyleSheet,
+  AppRegistry,
+  Text,
+  Button,
+  ScrollView,
+  Image,
+  View,
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+var styles = require('./build/styles.js');
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Yenivasita',
+  };
+  
+  render() {
+    const { navigate } = this.props.navigation;
+    
+    var json = require('./data/sample.json');
+    var data = json.data;
+    
+    var requests = [];
+    
+    for(var i=0; i<data.length; i++)
+    {
+      requests.push( <Request key={i} title={data[i].title} desc={data[i].rqs_text} location={data[i].cty_title} time={data[i].ago} /> )
+    }
+    
+    return (
+      
+      <View style={styles.ground}>
+      <ScrollView contentContainerStyle={styles.container}>
+      
+        {requests}
+      
+      </ScrollView>
+      </View>
+    );
+  }
+}
+
+class Request extends React.Component {
+  render() {
+    return (
+      <View style={styles.request}>
+        <View style={styles.requestLeft}>
+          <View style={styles.newIcon}>
+            <Text style={styles.newIconText}>Yeni</Text>
+          </View>
+        </View>
+        <View style={styles.requestCenter}>
+          <Text style={styles.medium}>{this.props.title}</Text>
+          <Text style={styles.small}>{this.props.desc}</Text>
+          <View style={styles.requestLocation}>
+            <Image source={require('./img/0.png')} style={{width:24, height:24}} />
+            <Text style={styles.blueText}>{this.props.location}</Text>
+          </View>
+        </View>
+        <View style={styles.requestRight}>
+          <Text style={[styles.tiny, styles.requestTime]}>{this.props.time}</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+function onPressLearnMore() {
+    alert(this.accessibilityLabel)
+}
+
+class DetailScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Hasan',
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <Button
+        title="Go Home"
+        onPress={() =>
+          navigate('Home')
+        }
+      />
+    );
+  }
+}
+
+const YenivasitaApp = StackNavigator({
+  Home: { screen: HomeScreen },
+  Detail: { screen: DetailScreen },
+});
+
+export default YenivasitaApp
---
+++ build/styles.js
@@ -0,0 +1,61 @@
+/* STYLES */
+'use strict';
+var React = require('react-native');
+var { StyleSheet, } = React;
+
+module.exports = StyleSheet.create({
+  
+  // ALL STYLES GO HERE
+  
+  ground:{
+    flex:1,
+    backgroundColor: "#F6F7F7",
+  },
+  container: {
+    backgroundColor: '#F6F7F7',
+    paddingBottom:50,
+  },
+  request: {
+    flex:1,
+    flexDirection: 'row',
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 10,
+    marginTop: 10,
+    minHeight: 150,
+    maxHeight: 150,
+    backgroundColor: '#ffffff',
+    shadowColor: '#000000',
+    shadowOffset:{width:1, height:1},
+    shadowOpacity:.2,
+    shadowRadius:2,
+  },
+      requestLeft:{flex:1, maxWidth:45},
+        newIcon:{top:25, width:35, height:20, backgroundColor:"#166FDB", borderTopRightRadius:10, borderBottomRightRadius:10, justifyContent: 'center', alignItems: 'center'},
+          newIconText:{fontSize:11, color:'#ffffff', backgroundColor:'transparent'},
+      requestCenter:{flex:1, marginTop:20},
+        requestLocation:{flex:1, flexDirection: 'row', alignItems:'flex-end', height:30},
+      requestRight:{flex:1, maxWidth:90},
+        requestTime:{top:25},
+  
+  //TEXT STYLES
+  tiny:{fontSize:14, color:'#777777'},
+  small:{fontSize:16, color:'#777777'},
+  medium:{fontSize:20, color:'#000000'},
+  blueText:{fontSize:13, color:'#4388D1'},
+  
+  thumbnail: {
+    width: 90,
+    height: 70,
+    borderRadius: 5,
+    margin: 5,
+  },
+  bigblue: {
+    color: '#FF0000',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red:{
+    backgroundColor:'red',
+  }
+});
